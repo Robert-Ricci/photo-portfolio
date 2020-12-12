39e29e97cf6e41850cd06f689046c9c58153d717
Robert Ricci <rmricci121@gmail.com>
2020-12-12 06:25:45 -0500

    This commit fixed app.js so that it ws displaying

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useState} from 'react';
-import ImageGrid from './components/ImageGrid';
+import './App.css';
 import Main from './components/Main';
-import Upload from './components/Upload';
-import Modal from './components/Modal';
 
 function App() {
-  
-  const[selectedImg, setSelectedImg] = useState(null)
-  
   return (
     <div className="App">
-      <Main/>
-      <Upload />
-      <ImageGrid  setSelectedImg = {setSelectedImg} />
-      {selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
+      <Main />
+
     </div>
   );
 }

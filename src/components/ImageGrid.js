8a862b7d27400ccd5036acb0e97755ea23daeba7
Robert Ricci <rmricci121@gmail.com>
2020-12-15 13:29:49 -0500

    This commit fizes the sizing issue with the images

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -8,7 +8,7 @@ const ImageGrid = ({ setSelectedImg }) => {
     console.log(docs)
 
     return (
-        <div className = "image-grid">
+        <div className = "img-grid">
             { docs && docs.map(doc => (
                 <div className="img-wrap" key={doc.id}
                 onClick = {() => setSelectedImg(doc.url)}>

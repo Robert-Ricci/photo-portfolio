af4f331703e6e14b389d97151fa1c18b5ff4fd68
Robert Ricci <rmricci121@gmail.com>
2020-12-22 15:29:12 -0500

    This commit adds css to the about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,17 @@
 import React from 'react';
-
+import Headshot from '../headshotcopy.jpg';
+import Image from 'react-bootstrap/Image';
 const About = () => {
     return (
-        <div>
-            <p> I'm baby master cleanse vaporware activated charcoal 8-bit, DIY lomo kickstarter subway tile man braid. Synth cronut lo-fi drinking vinegar, semiotics cold-pressed single-origin coffee. Mustache humblebrag bicycle rights raclette freegan, gentrify locavore aesthetic hashtag synth. Vice yr subway tile retro flannel tofu DIY plaid tousled wolf. Woke blue bottle green juice, palo santo banh mi marfa offal drinking vinegar vice. Shaman VHS plaid dreamcatcher vegan lumbersexual. Scenester narwhal selfies, before they sold out chartreuse 90's fixie single-origin coffee +1 swag succulents banh mi forage.
+        <div className="about-page">
+            <p className="about"> I'm baby master cleanse vaporware activated charcoal 8-bit, DIY lomo kickstarter subway tile man braid. Synth cronut lo-fi drinking vinegar, semiotics cold-pressed single-origin coffee. Mustache humblebrag bicycle rights raclette freegan, gentrify locavore aesthetic hashtag synth. Vice yr subway tile retro flannel tofu DIY plaid tousled wolf. Woke blue bottle green juice, palo santo banh mi marfa offal drinking vinegar vice. Shaman VHS plaid dreamcatcher vegan lumbersexual. Scenester narwhal selfies, before they sold out chartreuse 90's fixie single-origin coffee +1 swag succulents banh mi forage.
 
 Hell of microdosing lo-fi occupy mumblecore. Food truck gastropub seitan letterpress, copper mug williamsburg mumblecore pabst subway tile. Adaptogen raclette quinoa tilde portland ugh. Seitan irony hot chicken art party, fixie bicycle rights twee celiac cronut adaptogen. Sartorial chambray offal bespoke tacos gastropub, pork belly viral la croix mixtape.
 
 Raclette normcore hella listicle green juice snackwave af coloring book edison bulb pork belly adaptogen locavore +1 kinfolk yr. Gastropub seitan trust fund chambray. Pok pok food truck williamsburg flexitarian glossier beard brooklyn. Small batch tattooed enamel pin occupy viral.
 
 Dummy text? More like dummy thicc text, amirite?</p>
+        <Image className="headshot" src={Headshot} />
         </div>
     );
 };

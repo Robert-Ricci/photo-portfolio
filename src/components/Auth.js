8bee5fb39426e28ac7cda5459bd6fbe811d77d4c
Robert Ricci <rmricci121@gmail.com>
2021-02-20 10:10:31 -0500

    This commit adds google auth

---
+++ src/components/Auth.js
@@ -0,0 +1,26 @@
+const IfUnAuthed = () => {
+    return (
+      <div>
+        <h2>You're not signed in </h2>
+        <button
+          onClick={() => {
+            firebase
+              .app()
+              .auth()
+              .signInAnonymously();
+          }}
+        >
+          Sign in anonymously
+        </button>
+        <button
+          onClick={() => {
+            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(googleAuthProvider);
+          }}
+        >
+          Sign in with Google
+        </button>
+      </div>
+    );
+  };
+  

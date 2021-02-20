a5358989ff9f22ab86dcd364791cdce31085b87b
Robert Ricci <rmricci121@gmail.com>
2021-02-20 10:04:11 -0500

    Thsi commit adds firebase auth

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
+import "firebase/auth";
 import 'firebase/storage';
 import 'firebase/firestore';
+
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
